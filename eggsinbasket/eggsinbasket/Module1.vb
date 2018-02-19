6aa5671d052258f01570fc5faa53bba3da7ed720
hamzargc <34453653+hamzargc@users.noreply.github.com>
2018-02-18 22:51:04 -0800

    Add files via upload

---
+++ eggsinbasket/eggsinbasket/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Nofeggs, Boxes, eggsleft As Integer
+        Console.WriteLine("Input No of eggs : ")
+        Nofeggs = Console.ReadLine()
+        Call Eggsintoboxes(Nofeggs, Boxes, eggsleft)
+        Console.WriteLine("Boxes = " & Boxes & " EGGSLEFT = " & eggsleft)
+        Console.ReadKey()
+
+    End Sub
+
+
+    Sub Eggsintoboxes(ByRef NOE As Integer, ByRef B As Integer, ByRef EL As Integer)
+
+        B = NOE \ 6
+        EL = NOE Mod 6
+
+
+    End Sub
+
+End Module
